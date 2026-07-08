73d6126e360bbad54ec72a463ec9e64b7c3f74ac
Christopher <ch.welschoff@googlemail.com>
2026-07-08 16:41:53 +0200

    added new questions and JS

--- index.js
+++ index.js
@@ -5,5 +5,65 @@ button1.addEventListener("click", function () {
   answer1.classList.toggle("hidden");
 });
 
+const button2 = document.getElementById("showAnswer2");
+const answer2 = document.getElementById("answer-2");
 
-console.log()
+button2.addEventListener("click", function () {
+  answer2.classList.toggle("hidden");
+});
+
+const button3 = document.getElementById("showAnswer3");
+const answer3 = document.getElementById("answer-3");
+
+button3.addEventListener("click", function () {
+  answer3.classList.toggle("hidden");
+});
+
+const button4 = document.getElementById("showAnswer4");
+const answer4 = document.getElementById("answer-4");
+
+button4.addEventListener("click", function () {
+  answer4.classList.toggle("hidden");
+});
+
+const button5 = document.getElementById("showAnswer5");
+const answer5 = document.getElementById("answer-5");
+
+button5.addEventListener("click", function () {
+  answer5.classList.toggle("hidden");
+});
+
+const button6 = document.getElementById("showAnswer6");
+const answer6 = document.getElementById("answer-6");
+
+button6.addEventListener("click", function () {
+  answer6.classList.toggle("hidden");
+});
+
+const button7 = document.getElementById("showAnswer7");
+const answer7 = document.getElementById("answer-7");
+
+button7.addEventListener("click", function () {
+  answer7.classList.toggle("hidden");
+});
+
+const button8 = document.getElementById("showAnswer8");
+const answer8 = document.getElementById("answer-8");
+
+button8.addEventListener("click", function () {
+  answer8.classList.toggle("hidden");
+});
+
+const button9 = document.getElementById("showAnswer9");
+const answer9 = document.getElementById("answer-9");
+
+button9.addEventListener("click", function () {
+  answer9.classList.toggle("hidden");
+});
+
+const button10 = document.getElementById("showAnswer10");
+const answer10 = document.getElementById("answer-10");
+
+button10.addEventListener("click", function () {
+  answer10.classList.toggle("hidden");
+});
