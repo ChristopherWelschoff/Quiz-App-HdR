be23480e5f97823335fc3f4d02adb5dfe3643baf
Christopher <ch.welschoff@googlemail.com>
2026-07-08 12:44:46 +0200

    added some js for practice

---
+++ index.js
@@ -0,0 +1,9 @@
+const button1 = document.getElementById("showAnswer1");
+const answer1 = document.getElementById("answer-1");
+
+button1.addEventListener("click", function () {
+  answer1.classList.toggle("hidden");
+});
+
+
+console.log()
