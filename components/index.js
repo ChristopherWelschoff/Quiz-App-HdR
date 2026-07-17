8aa5d7bde02c26f586fe7e18fcc34df8ae35ec65
Christopher <ch.welschoff@googlemail.com>
2026-07-17 10:04:09 +0200

    add toggle hide answer text if answer is shown

--- components/index.js
+++ components/index.js
@@ -1,24 +1,27 @@
 const answerButton = document.querySelector('[data-js="answer-button1"]');
 
-
 const answers = document.querySelector('[data-js="hidden"]');
 
+const showHideAnswer = document.querySelector('[data-js="show-answer"]')
 
 answerButton.addEventListener("click", () => {
   answers.classList.toggle("hidden");
+
+  if (answers.classList.contains("hidden")) {
+    showHideAnswer.textContent = "Antwort zeigen ▼";
+  } else {
+  showHideAnswer.textContent = "Antwort verbergen ▲";
+  }
 });
 
 console.clear();
 
 const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
 
-
 const bookmarkSaved = document.querySelector('[data-js="bookmark-img"]');
-
 
 bookmarkButton.addEventListener("click", () => {
   bookmarkSaved.classList.toggle("bookmark-icon");
 });
 
 console.clear();
-
